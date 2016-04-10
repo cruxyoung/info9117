805c30846376737d2bca261af70cbb008bd52617
crux <yangjie9176@126.com>
2016-04-09 22:07:56 -0700

    all-bdd-unittest

--- bluetest_test.py
+++ bluetest_test.py
@@ -11,6 +11,7 @@ class BlueGardenTest(unittest.TestCase):
         self.app = bluetest.app.test_client()
         bluetest.init_db()
         
+        
     def login(self,username, password):
         return self.app.post('/login', data = dict(
         username=username,
@@ -18,12 +19,38 @@ class BlueGardenTest(unittest.TestCase):
         
         ), follow_redirects=True)
         
+    def register(self, username, password, password2):
+        return self.app.post('/register', data=dict(
+        username=username,
+        password=password,
+        password2=password2
+        ),follow_redirects=True)
+        
+        
     def logout(self):
         return self.app.get('/logout', follow_redirects=True)
         
     def test_login_logout(self):
         rv=self.login('123','123')
         assert 'You were logged in' in rv.data
+        rv=self.logout()
+        assert 'You were logged out' in rv.data
+        rv=self.login('yangyang', 'yang')
+        assert 'Invalid username' in rv.data
+        rv=self.login('yang', 'ya')
+        assert 'Invalid password' in rv.data
+        
+    def test_register(self):
+        
+        rv=self.register('yan','pwyan', 'pwyan')
+        assert 'You were successfully registered and can login now' in rv.data
+        rv=self.register('yan','pwyan', 'pwyn')
+        assert 'The two passwords do not match' in rv.data
+        rv=self.register('yan','', '')
+        assert 'You have to enter a password' in rv.data
+        rv=self.register('iin','fsd', 'fafs')
+        assert 'The two passwords do not match' in rv.data
+        
         
         
         
